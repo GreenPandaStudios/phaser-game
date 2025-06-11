5afd35599879197e69ade7ec04b5c2422b0885e7
greenpandastudios <augustamiller77@gmail.com>
2025-06-10 19:42:34 -0500

    Change to Ballgust

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const App = () => {
     <div className="app-wrapper">
       <nav className="navbar">
         <div className="navbar-container">
-          <h1 className="navbar-logo">Game</h1>
+          <h1 className="navbar-logo">Ballgust</h1>
           <ul className="navbar-menu">
             <li className="navbar-item">
-              <Link to="/" className="navbar-link">Game</Link>
+              <Link to="/" className="navbar-link">Play</Link>
             </li>
             <li className="navbar-item">
               <Link to="/leaderboard" className="navbar-link">Leaderboard</Link>
